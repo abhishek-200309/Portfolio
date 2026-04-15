9311eaa07c6ab91a3145af5a15813729cd75a48a
Abhishek Kumar <abhishek08diwakar@gmail.com>
2026-04-15 16:37:01 +0530

    try again

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useRef, useState } from 'react';
+// import { useState } from 'react';
 import type { MediaStore } from '../types';
 import { evProjectDashboard } from '../data/evDashboardData';
 
